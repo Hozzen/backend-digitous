60a748388a4a02693f6389b623a4269d53e5c4e3
Kemayou Kévin <kemayou.ke@gmail.com>
2021-07-06 15:03:58 +0200

    Exercices 1 and 2

--- chapitre_01/authors/index.js
+++ chapitre_01/authors/index.js
@@ -1,7 +1,56 @@
 const express = require('express');
 const app = express();
 
-const port = 8000;
-app.listen(port, () => {
-    console.log('Server started on port: ' + port)
+const port = 8000
+
+var authors = [
+    {
+        name: "Lawrence Nowell",
+        nationality: "UK",
+        books: ["Beowulf"]
+    },
+    {
+        name: "William Shakespeare",
+        nationality: "UK",
+        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+    },
+    {
+        name: "Charles Dickens",
+        nationality: "US",
+        books: ["Oliver Twist", "A Christmas Carol"]
+    },
+    {
+        name: "Oscar Wilde",
+        nationality: "UK",
+        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+    },
+]
+
+// Exercice 1 
+
+app.get("/", (req, res) => {
+    res.send("Authors API")
+});
+
+// Exercice 2 
+
+app.get("/authors/:number", (req, res) => {
+    let number = req.params.number
+    res.send(`${authors[number - 1].name}, ${authors[number - 1].nationality}`)
+
+});
+
+// Exercice 3 
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () =>  {
+    console.log("Server started on port: " + port);
 });
