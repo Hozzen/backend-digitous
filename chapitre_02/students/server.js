497355739851b65f2fb0269cab49d2b04a82548e
Kemayou Kévin <kemayou.ke@gmail.com>
2021-08-31 19:48:46 +0200

    Chapitre_02 students exercise done (without bonuses yet)

--- chapitre_02/students/server.js
+++ chapitre_02/students/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 
 const students = [
   {
@@ -14,12 +15,35 @@ const students = [
   },
 ];
 
+// Permet de récupérer le body
+app.use(express.json());
+
+app.use(cors());
+
+
+
+// Middleware global
+app.use((req, res, next) => {
+  console.log("middleware ok");
+  next();
+});
+
+
+
 app.get("/students", (req, res) => {
   res.json(students);
 });
 
 app.post("/students", (req, res) => {
-  res.json({});
+  const newStudent = req.body;
+  console.log(newStudent)
+  let newStudentsList = students.push(newStudent)
+  console.log(newStudentsList)
+  
+  res.json({
+    message: "Student bien ajouté !",
+    students: students,
+  });
 });
 
 app.listen(PORT, () => {
