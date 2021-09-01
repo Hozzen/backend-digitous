4f8ea532ab508a8ee29857e10d22778b78247ff6
Kemayou Kévin <kemayou.ke@gmail.com>
2021-09-01 15:36:32 +0200

    upload progress

--- chapitre_03/upload/server.js
+++ chapitre_03/upload/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const PORT = 3000;
+
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "status",
-  });
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server listen on ${PORT}`);
