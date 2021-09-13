273bf53b1e73db5d7735375e990ea6ca5587d0d1
Kemayou Kévin <kemayou.ke@gmail.com>
2021-09-13 21:57:40 +0200

    Initial commit

---
+++ morning_populate/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(console.log("connected to MongoDB"));
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log("Server listen on port : ", process.env.PORT);
+});
